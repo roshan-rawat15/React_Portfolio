21710e00f4f9ff4147cce1e1a2c4cdad52f70955
Roshan Kumar Rawat <165247276+Roshankumarrawat@users.noreply.github.com>
2024-10-24 00:00:12 +0530

    Fix line endings in .gitignore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from './containers/home' // Adjust the path as necessary
+import About from './containers/about'; // Adjust the path as necessary
+import Resume from './containers/resume'; // Adjust the path as necessary
+import Skills from './containers/skills'; // Adjust the path as necessary
+import Portfolio from './containers/portfolio'; // Adjust the path as necessary
+import Contact from './containers/contact'; // Adjust the path as necessary
+import Navbar from "./components/navBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* particles js */}
+
+      {/* navbar */}
+      <Navbar/>
+
+      {/* main page content */}
+
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
