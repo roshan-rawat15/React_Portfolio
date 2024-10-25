65a0bb104046bbe46339a47c609da5d827da1492
Roshan Kumar Rawat <165247276+Roshankumarrawat@users.noreply.github.com>
2024-10-25 19:30:03 +0530

    home Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import "./App.scss";
+import { Routes, Route, useLocation } from "react-router-dom";
+// import Particles from "react-tsparticles";
+// import { loadFull } from "tsparticles";
 import Home from './containers/home' // Adjust the path as necessary
 import About from './containers/about'; // Adjust the path as necessary
 import Resume from './containers/resume'; // Adjust the path as necessary
@@ -7,17 +9,36 @@ import Skills from './containers/skills'; // Adjust the path as necessary
 import Portfolio from './containers/portfolio'; // Adjust the path as necessary
 import Contact from './containers/contact'; // Adjust the path as necessary
 import Navbar from "./components/navBar";
+// import particlesOptions from './utils.js/particles';// Import the particles options
 
 function App() {
+
+ const location = useLocation();
+ console.log(location);
+ 
+
+  // const handleInit = async (main)=>{
+  //   await loadFull(main)
+  // }
+
+    const renderPartclesJsInHomePage = location.pathname === "/";
+
   return (
     <div className="App">
       {/* particles js */}
+
+      {
+        renderPartclesJsInHomePage
+      }
+
+      {/* <Particles id="particles" options={particlesOptions} init={handleInit} /> */}
 
       {/* navbar */}
       <Navbar/>
 
       {/* main page content */}
 
+      <div className="App__main-page-content">
       <Routes>
         <Route index path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -26,6 +47,7 @@ function App() {
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      </div>
     </div>
   );
 }
--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Animate } from "react-simple-animate";
+import "./styles.scss";
 
-const Home =()=>{
-    return(
-        <div>
-            Home page
+const Home = () => {
+  const navigate = useNavigate();
+
+  const handleNavigateToContactPage = () => {
+    navigate("/contact");
+  };
+
+  return (
+    <section id="home" className="home">
+      <div className="home__text-wrapper">
+        <h1>
+          Hello, I'm Roshan Kumar <br />
+          And I'm a <br />
+          Full Stack Developer & UI/UX Designer
+        </h1>
+      </div>
+
+      <Animate  
+      play
+      duration={1.5}
+      delay={1}
+      start={{
+        transform : 'translateY(550px)'
+      }}
+
+      end={{
+        transform : 'translatex(0px)'
+      }}
+      
+      
+      >
+        <div className="home__text-wrapper__contact-me">
+          <button onClick={handleNavigateToContactPage}>Hire Me</button>
         </div>
-    )
-}
+      </Animate>
+    </section>
+  );
+};
 
 export default Home;
