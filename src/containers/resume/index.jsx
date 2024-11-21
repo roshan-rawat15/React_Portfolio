cc6f27bfee75a3799f92807863fd33353174cf9f
Roshan Kumar Rawat <165247276+Roshankumarrawat@users.noreply.github.com>
2024-11-21 14:58:09 +0530

    resume

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -1,17 +1,76 @@
-import React from 'react';
-import { BsInfoCircleFill } from 'react-icons/bs';
-import PageHeaderContent from '../../components/pageHeaderContent';
+import React from "react";
+import { BsInfoCircleFill } from "react-icons/bs";
+import PageHeaderContent from "../../components/pageHeaderContent";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { data } from "./utils";
+import './styles.scss';
 
-const Resume =()=>{
-    return(
-        <section id='resume' className='resume'>
+const Resume = () => {
+  return (
+    <section id="resume" className="resume">
+      <PageHeaderContent
+        headerText="My Resume"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="timeline">
+        <div className="timeline__experience">
+          <h3 className="timeline__experience__header-text">Experience</h3>
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {data.experience.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__experience__vertical-timeline-element"
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3 className="vertical-timeline-element-title">
+                    {item.title}
+                  </h3>
 
-           <PageHeaderContent 
-           headerText = "My Resume"
-           icon={<BsInfoCircleFill size={40}/>}
-           />
-       </section>
-    )
-}
+                  <h4 className="vertical-timeline-element-subtitle">
+                    {item.subTitle}
+                  </h4>
+                  <p>{item.description}</p>
+                </div>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+        <div className="timeline__education">
+          <h3 className="timeline__education__header-text">Education</h3>
+
+          <VerticalTimeline
+            layout={"1-column"}
+            lineColor="var(--yellow-theme-main-color)"
+          >
+            {data.education.map((item, i) => (
+              <VerticalTimelineElement
+                key={i}
+                className="timeline__experience__vertical-timeline-element"
+              >
+                <div className="vertical-timeline-element-title-wrapper">
+                  <h3 className="vertical-timeline-element-title">
+                    {item.title}
+                  </h3>
+
+                  <h4 className="vertical-timeline-element-subtitle">
+                    {item.subTitle}
+                  </h4>
+                  <p>{item.description}</p>
+                </div>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Resume;
---
+++ src/containers/resume/utils.js
@@ -0,0 +1,33 @@
+export const data = {
+    experience: [
+        {
+            title: "Intern – Web Development",
+        subTitle: "Contributed to front-end development and API integration for client projects",
+        description: "Designed and implemented a dynamic e-commerce platform using ReactJS, Node.js, and MongoDB.",
+        },
+        {
+            title: "Coding Challenges and Competitive Programming",
+            subtitle: "Solving complex problems on LeetCode",
+            description: "Solved problems related to data structures, algorithms, and problem-solving",
+
+        },
+    ],
+
+education: [
+    {
+        title: "Secondary",
+        subTitle: "D.V.C +2 High School",
+        description: "10th Grade ",
+    },
+    {
+        title: "Higher Secondary",
+        subTitle: "D.V.C +2 High School",
+        description: "12th Grade in Science",
+    },
+    {
+        title: "BCA",
+        subTitle: "Greater Noida Institute of Technology",
+        description: "Bechelor of computer application ",
+    }
+],
+};
